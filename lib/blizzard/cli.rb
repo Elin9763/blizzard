d7e1ef5a1c50d9fdc8c01ba0cdd01020d01e7807
Eric Lin <esotericlin@gmail.com>
2018-01-13 19:55:51 +0000

    added goodbye method to cli

--- lib/blizzard/cli.rb
+++ lib/blizzard/cli.rb
@@ -2,12 +2,13 @@
 class Blizzard::CLI
 
   def call
-    puts "Blizzard's Games:"
     list_games
     menu
+    goodbye
   end
 
   def list_games
+    puts "Blizzard's Games:"
     puts <<~DOC
       1. Overwatch
       2. Hearthstone
@@ -19,25 +20,33 @@ class Blizzard::CLI
   end
 
   def menu
-    puts "Enter the number of the game you'd like to know about:"
-    input = gets.strip
-    case input
-    when "1"
-      puts "You've chosen Overwatch"
-    when "2"
-      puts "You've chosen Hearthstone"
-    when "3"
-      puts "You've chosen World of Warcraft"
-    when "4"
-      puts "You've chosen Heroes of the Storm"
-    when "5"
-      puts "You've chosen Starcraft II"
-    when "6"
-      puts "You've chosen Diablo III"
+    input = nil
+    while input != "exit"
+      puts "Enter the number of the game you'd like to know about OR type list to see the games again OR type exit to quit:"
+      input = gets.strip.downcase
+      case input
+      when "1"
+        puts "You've chosen Overwatch"
+      when "2"
+        puts "You've chosen Hearthstone"
+      when "3"
+        puts "You've chosen World of Warcraft"
+      when "4"
+        puts "You've chosen Heroes of the Storm"
+      when "5"
+        puts "You've chosen Starcraft II"
+      when "6"
+        puts "You've chosen Diablo III"
+      when "list"
+        list_games
+      else
+        puts "Not an option, type list to go back to game list and choose again"
+      end
     end
+  end
 
-
-
+  def goodbye
+    puts "Please come again!"
   end
 
 end
